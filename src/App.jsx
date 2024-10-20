53ef8329615b8a9ccb7a8e73a12bc033e98ab821
Archit Saini <archit.as.802@gmail.com>
2024-10-20 19:45:32 +0530

    Use jsx and update EsLint for react

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,37 @@
-import React from "react";
+//import React from "react";
 import { createRoot } from "react-dom";
 
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
+import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", { id: "my-div" }, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Sheero",
-      animal: "Dog",
-      breed: "Pug",
-    }),
-    React.createElement(Pet, {
-      name: "Shadow",
-      animal: "Dog",
-      breed: "Poodle",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Parrot",
-    }),
-  ]);
+  // return React.createElement("div", { id: "my-div" }, [
+  //   React.createElement("h1", {}, "Adopt Me!"),
+  //   React.createElement(Pet, {
+  //     name: "Sheero",
+  //     animal: "Dog",
+  //     breed: "Pug",
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "Shadow",
+  //     animal: "Dog",
+  //     breed: "Poodle",
+  //   }),
+  //   React.createElement(Pet, {
+  //     name: "Pepper",
+  //     animal: "Bird",
+  //     breed: "Parrot",
+  //   }),
+  // ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Sheero" animal="Dog" breed="Pug"></Pet>
+      <Pet name="Shadow" animal="Dog" breed="Poodle"></Pet>
+      <Pet name="Pepper" animal="Bird" breed="Parrot"></Pet>
+    </div>
+  );
 };
 const container = document.getElementById("root");
 const root = createRoot(container);
-root.render(React.createElement(App, {}, null));
+//root.render(React.createElement(App, {}, null));
+root.render(<App />);
